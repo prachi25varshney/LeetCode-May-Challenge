8bb3264a245b8e8ebd10ee6bd1bb7a02e23fa148
prachi25varshney <65917234+prachi25varshney@users.noreply.github.com>
2021-05-15 19:23:20 +0530

    Create Valid Number.java

---
+++ Valid Number.java
@@ -0,0 +1,62 @@
+/*Question :
+A valid number can be split up into these components (in order):
+
+A decimal number or an integer.
+(Optional) An 'e' or 'E', followed by an integer.
+A decimal number can be split up into these components (in order):
+
+(Optional) A sign character (either '+' or '-').
+One of the following formats:
+At least one digit, followed by a dot '.'.
+At least one digit, followed by a dot '.', followed by at least one digit.
+A dot '.', followed by at least one digit.
+An integer can be split up into these components (in order):
+
+(Optional) A sign character (either '+' or '-').
+At least one digit.
+For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
+
+Given a string s, return true if s is a valid number. */
+
+
+
+//Solution 
+
+import java.util.regex.*;
+class Solution {
+    public boolean isNumber(String s) {
+        int len = s.length();
+        int i = 0, e = len - 1;
+        while (i <= e && Character.isWhitespace(s.charAt(i))) i++;
+        if (i > len - 1) return false;
+        while (e >= i && Character.isWhitespace(s.charAt(e))) e--;
+        // skip leading +/-
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') i++;
+        boolean num = false; // is a digit
+        boolean dot = false; // is a '.'
+        boolean exp = false; // is a 'e'
+        while (i <= e) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = true;
+            }
+            else if (c == '.') {
+                if(exp || dot) return false;
+                dot = true;
+            }
+            else if (c == 'e' || c== 'E') {
+                if(exp || num == false) return false;
+                exp = true;
+                num = false;
+            }
+            else if (c == '+' || c == '-') {
+                if (s.charAt(i - 1) != 'e') return false;
+            }
+            else {
+                return false;
+            }
+            i++;
+        }
+        return num;   
+    }
+}
